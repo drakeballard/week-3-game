64cc3f5b974cd5c6588b2c608de7f19fd0688c37
Drake Ballard <drakeballard@gmail.com>
2016-08-19 17:31:50 -0500

    html layout is done. Working on js. pseudocode. currently confused about the array setup.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,7 @@
-//Instruction phase
+//pseudocode for psychic game
 
-//1. word is chosen at random from a word bank (which is already created)
-//word = wordbank[randomNum]
-//wordArray = word.split("") <-- turn word into wordArray
-//2. user has a certain number of guesses based on character count of word
-//numGuesses = wordsize
-//3. user types letters to start guessing
-//check if userGuess is in wordArray. if letter is found in word:
-// find index of letter in wordArray (if yes, add userGuess to blankLetter at index i)
-// if user guesses remaining letts correctly, user wins
-// else, if user guesses letters incorrectly they get a strike
-// else if the remainder of tries are elimanted, then the user looses
+//need to decalre certain variables for this game to initiate
 
+//since their are 26 letters in the alphabet. They have to be put into an array. Does each letter have to be in its own array?
+
+//
