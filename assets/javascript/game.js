aca0f7aedb6edbcc965a407845483d003787dd15
Drake Ballard <drakeballard@gmail.com>
2016-08-19 17:36:51 -0500

    html layout is done. Working on js. pseudocode. currently confused about the array setup. pseudo looks a bit long. will have to categorize certain commands.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,4 +4,5 @@
 
 //since their are 26 letters in the alphabet. They have to be put into an array. Does each letter have to be in its own array?
 
-//
+//The game can be initiated with the following factors to be considered:  the computer needs to pick (generate) a randome letter our of 26. After computer selects  the letter, it needs to remember (recall) it.
+
