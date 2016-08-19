cbe3ebb081c356769e8c510a19a74b0eb5fff293
Drake Ballard <drakeballard@gmail.com>
2016-08-19 17:42:11 -0500

    html layout is done. Working on js. pseudocode working progress. filling the code now.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,5 +4,10 @@
 
 //since their are 26 letters in the alphabet. They have to be put into an array. Does each letter have to be in its own array?
 
-//The game can be initiated with the following factors to be considered:  the computer needs to pick (generate) a randome letter our of 26. After computer selects  the letter, it needs to remember (recall) it.
+//The game can be initiated with the following factors to be considered:  
+	//the computer needs to pick (generate) a randome letter out of 26. 
+	//After computer selects  the letter, it needs to remember (recall) it.
+	//The EndUser can now start guessing the stored "letter" that the computer selects. In html i indicated that EndUser only gets 10 tries
+	//After the 10 tries, the computer gets to reveal the letter that was selected but gets to select a new letter by reseting itself
+
 
