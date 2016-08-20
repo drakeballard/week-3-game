94a14538b9193c695e60367f4ba77e9eac2eecaa
Drake Ballard <drakeballard@gmail.com>
2016-08-19 19:09:02 -0500

    forgot to add the javascript source in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,7 +3,7 @@
 //need to decalre certain variables for this game to initiate
 	var computerGuess,
 		EndUserGuess,
-		EndUserGuesses
+		EndUserGuesses = [],
 		letters,
 		wins = 0,
 		losses = 0,
@@ -12,22 +12,24 @@
 		html;
 
 //since their are 26 letters in the alphabet. They have to be put into an array. Does each letter have to be in its own array?
-	letters = abcdefghijklmnopqrstuvxyz
+	//This needs to split some how?
+
+	letters = ("abcdefghijklmnopqrstuvxyz").split("");
 
 //The game can be initiated with the following factors to be considered:  
 	function init () {
 
 	//the computer needs to pick (generate) a randome letter out of 26. 
-		computerGuess = Math.floor(Math.random()*letters);
+		computerGuess = Math.floor(Math.random()*letters.length);
 
 	//After computer selects  the letter, it needs to remember (recall) it.
-		computerGuess = randomNum
+		computerGuess = letters[randomNum];
 
 	//The EndUser can now start guessing the stored "letter" that the computer selects. In html i indicated that EndUser only gets 10 tries
 		guessCount = 10;
 
 	//After the 10 tries, the computer gets to reveal the letter that was selected but gets to select a new letter by reseting itself
-		EndUserGuesses = x;
+		EndUserGuesses = [];
 }
 
 // reset all the variables
